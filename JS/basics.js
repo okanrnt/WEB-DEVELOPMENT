fb843775f3cbb26a566b966b5456310434c874ae
Okan Oztan <okanrnt1@gmail.com>
2022-08-06 09:05:01 +0300

    basics document is added again

---
+++ JS/basics.js
@@ -0,0 +1,58 @@
+// CONSOLE
+console.log("Hello World")
+console.log("Okan Öztan")
+console.log(2.2)
+console.log(true)
+
+
+// DATA TYPES
+let namee = "Okan";
+console.log(namee);
+console.log(typeof namee);
+
+let age = 32;
+console.log(age);
+console.log(typeof age);
+
+let testing = 6>3;
+console.log(testing);
+console.log(testing);
+
+let a = 12;
+let b = 7;
+let result = a + b;
+let trying = result > 16;
+console.log(trying);
+console.log(typeof trying);
+
+let c = null;
+console.log(c);
+console.log(typeof c);
+
+let d;
+console.log(d);
+console.log(typeof d);
+
+
+let human = {
+    firstname : "Okan",
+    lastname : "Öztan",
+    agee : 32 
+}
+
+console.log(human);
+console.log(typeof human);
+
+
+let array = [1,2,3,4,5,6,7,8];
+
+console.log(array);
+console.log(typeof array);
+
+
+let func = function(){
+    console.log("Hello World");
+}
+
+func();
+console.log(typeof func);
